28abd6bb377f7966b57da889b070d92baa573df8
timf34 <farrelti@tcd.ie>
2023-08-06 14:16:56 +0100

    removed JetsonWiFiManger; download and build aws-iot-device-client; change permissions of aws iot certs; setup the git pull service;

--- initial_jetson_setup.sh
+++ initial_jetson_setup.sh
@@ -17,13 +17,6 @@ if [ ! -d "FOVCamerasWebApp" ]; then
     sudo chown -R $USER: FOVCamerasWebApp
 fi
 
-# Check if JetsonWiFiManager directory exists
-if [ ! -d "JetsonWiFiManager" ]; then
-    # Clone the JetsonWiFiManager repository
-    git clone https://github.com/timf34/JetsonWiFiManager.git
-    sudo chown -R $USER: FOVCamerasWebApp
-fi
-
 # Install pip
 sudo apt update
 sudo apt install python3-pip -y
@@ -32,6 +25,7 @@ sudo apt install python3-pip -y
 sudo apt install nano
 sudo apt install cmake
 sudo apt install libssl-dev
+sudo apt install network-manager
 
 
 pip3 install -r requirements.txt
@@ -42,12 +36,36 @@ pip3 install -r nano_requirements.txt
 
 # Create a .env file 
 touch .env
-echo "REACT_APP_URL=http://localhost:5000" > .env  # For development
-# echo "REACT_APP_URL=http://fovcameraswebappv2.eu-west-1.elasticbeanstalk.com" >> .env  # For production
+# echo "REACT_APP_URL=http://localhost:5000" > .env  # For development
+echo "REACT_APP_URL=http://fovcameraswebappv2.eu-west-1.elasticbeanstalk.com" >> .env  # For production
 
 
-# Install JetsonWiFiManager dependencies
-cd ~/Desktop/JetsonWiFiManager
-pip3 install -r requirements.txt
-chmod +x install_packages.sh
-./install_packages.sh
+# ------------------- Download and build aws-iot-device-client ----------------------------
+cd ~/Desktop
+git clone https://github.com/timf34/aws-iot-device-client
+cd aws-iot-device-client
+mkdir build
+cd build
+cmake ../
+cmake --build . --target aws-iot-device-client
+
+# Run the setup after this initial install script as we need to do so interactively as root 
+# cd .. 
+# sudo ./setup.sh
+
+# ---------- Change permissions of the aws iot device certificates ------------ (assuming they've already been downloaded)
+cd ~
+chmod 700 aws-iot-certs/
+cd aws-iot-certs
+chmod 600 private.pem.key
+chmod 644 certificate.pem.crt
+chmod 644 AmazonRootCA1.pem
+
+# ---------- Setup the git pull service ------------
+cd ~/Desktop/JetsonConfig/git_pull_service
+chmod +x git_pull.sh
+chmod +x git_pull_setup.sh
+./git_pull_setup.sh
+
+
+# ----------- Setup the device shadow service ------------
