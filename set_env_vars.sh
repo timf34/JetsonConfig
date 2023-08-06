2bd1caec2d5adca2332620fe4cfb1f6110d95985
timf34 <farrelti@tcd.ie>
2023-08-06 15:36:07 +0100

    made set_env_vars.sh more malleable

--- set_env_vars.sh
+++ set_env_vars.sh
@@ -4,18 +4,13 @@
 ENV_VAR_NAME="DEVICE_NAME"
 ENV_VAR_VALUE="jetson1"  # replace this with the desired device name
 
-# Check if the environment variable is already in /etc/environment
-if grep -q "$ENV_VAR_NAME" /etc/environment; then
-    echo "Environment variable $ENV_VAR_NAME already exists."
-else
-    # Add the environment variable to /etc/environment
-    echo "$ENV_VAR_NAME=\"$ENV_VAR_VALUE\"" | sudo tee -a /etc/environment
+# Add the environment variable to /etc/environment
+echo "$ENV_VAR_NAME=\"$ENV_VAR_VALUE\"" | sudo tee -a /etc/environment
 
-    # Export the variable in the current shell
-    export $ENV_VAR_NAME=$ENV_VAR_VALUE
+# Export the variable in the current shell
+export $ENV_VAR_NAME=$ENV_VAR_VALUE
 
-    echo "Environment variable $ENV_VAR_NAME added successfully."
-fi
+echo "Environment variable $ENV_VAR_NAME added successfully."
 
 # Print the value of the environment variable
 echo "$ENV_VAR_NAME = $DEVICE_NAME"
